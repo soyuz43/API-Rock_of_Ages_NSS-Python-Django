5448aca8e49655a4c3fd2c3094f50784330c3c99
William R. Stetar <kebekad673@proton.me>
2024-06-17 12:18:59 -0500

    feat: Expand Rock Serializer to Include User and Type Information

--- rockapi/serializers.py
+++ rockapi/serializers.py
@@ -1,29 +1,29 @@
-# File Path: API-Rock_of_Ages_NSS-Python-Django/rockapi/serializers.py
+# ! File Path: API-Rock_of_Ages_NSS-Python-Django/rockapi/serializers.py
 
 from rest_framework import serializers
 from rockapi.models import Rock, Type
-from django.contrib.auth.models import User  # ! Added import for User model
+from django.contrib.auth.models import User
 
 class RockTypeSerializer(serializers.ModelSerializer):
     """JSON serializer for rock type"""
 
     class Meta:
         model = Type
-        fields = ('label',)  # ! Include only the label field
+        fields = ('label',)
 
 class UserSerializer(serializers.ModelSerializer):
     """JSON serializer for user"""
 
     class Meta:
         model = User
-        fields = ('first_name', 'last_name')  # ! Include first and last name fields
+        fields = ('first_name', 'last_name')
 
 class RockSerializer(serializers.ModelSerializer):
-    """JSON serializer for rock"""
-
-    type = RockTypeSerializer(many=False)  # ! Use RockTypeSerializer for the type field
-    user = UserSerializer(many=False)  # ! Use UserSerializer for the user field
+    """JSON serializer for rocks"""
+    
+    type = RockTypeSerializer(many=False)  # ! Include type serializer
+    user = UserSerializer(many=False)  # ! Include user serializer
 
     class Meta:
         model = Rock
-        fields = ('id', 'name', 'weight', 'user', 'type')  # ! Updated fields to include 'user' and 'type'
+        fields = ('id', 'name', 'weight', 'user', 'type')  # ! Updated fields
